7622967a3f69125f29dbd69f21e748e55eefd266
Damain Ramsajan <dpramsajan@gmail.com>
2021-02-10 10:33:57 +0100

    Original Netflix app and all dependencies setup

--- src/App.js
+++ src/App.js
@@ -1,56 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import Row from "./Row";
+import requests from "./requests";
+import Banner from "./Banner";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="app">
+      
+      <Banner />
+      <Row 
+      title = "NETFLIX ORIGINALS" 
+      fetchUrl={requests.fetchNetflixOriginals}
+      isLargeRow
+       />
+      <Row title = "Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title = "Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title = "Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title = "Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title = "Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title = "Romantic Movies" fetchUrl={requests.fetchRomanticMovies} />
+      <Row title = "Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      <div className = "footer">
+        <p className = "contact__info">
+          <h2>Damain Peter Ramsajan</h2>
+          <h2>peterdramsajan@gmail.com</h2>
         </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+               
+      </div>
+      
+     
     </div>
   );
 }
